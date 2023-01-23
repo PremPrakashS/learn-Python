5917da2ecc24a47947ab7c57add3de8ec15a0f96
Prem Prakash <premp7420@gmail.com>
2023-01-23 18:20:35 +0100

    100 days of code python course

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# Alt + Shift + Up/Down : to replicate the selected line (copy the line) in VS code.
+
+a = 6
+b = 3
+print("The sum of", a , "and", b, "is :", a + b)
+print("The difference of", a , "and", b, "is :", a - b)
+print("The multiplication of", a , "and", b, "is :", a * b)
+print("The division of", a , "and", b, "is :", a / b)
+print("The floor division of", a , "and", b, "is :", a // b)
+print("The mod division of", a , "and", b, "is :", a % b)
+print("The  of", a , "and", b, "is :", a ** b)
---
+++ String.py
@@ -0,0 +1,10 @@
+string = '''This is a
+multiline
+string'''
+
+string1 = 'This is a \
+multiline \
+string'
+
+for i in string1:
+    print(i)
