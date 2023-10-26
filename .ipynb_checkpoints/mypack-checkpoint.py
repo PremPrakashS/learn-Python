ebe28a98c94be01cde9adbd1b819becdb9839a2d
PremPrakashS <premp7420@gmail.com>
2023-10-26 14:13:18 +0200

    Added all the files

---
+++ .ipynb_checkpoints/mypack-checkpoint.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("Hey you are welcome from Harry")
+
+# print(__name__)  
+# welcome()
+  
+if __name__== "__main__":
+    print(__name__)  
+    welcome()
---
+++ prem.py
@@ -0,0 +1,5 @@
+def welcome():
+    print("Hey you are welcome my friend")
+    
+    
+prem = "A good boy"
